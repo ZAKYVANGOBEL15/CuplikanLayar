91083a54ca4d66695619cb1faacdda35397409f2
Zaky <145343615+ZAKYVANGOBEL15@users.noreply.github.com>
2025-02-07 14:47:07 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'sm-md': '1074px', // Menambahkan breakpoint khusus di 1074px
+        },
+      },
+    },
+  }
+  
